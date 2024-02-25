62fc3f75ad0ac1b6b20aeeddbfffc0f0e0f5c00c
Philani Ngcamu <Philaningcamu18@gmail.com>
2024-02-25 22:29:09 +0200

    fetching student data in the backend

--- js/admin_dashboard.js
+++ js/admin_dashboard.js
@@ -1,5 +1,6 @@
 import * as Utility from "./admin_dashboard_utility.js";
 import * as Institute from "./institutes.js";
+import * as Student from "./students.js";
 
 
 function fetchAllInstitutes(statusId) {
@@ -38,6 +39,40 @@ function fetchAllInstitutes(statusId) {
     })
 }
 
+function fetchStudents(){
+    let loginDetails = Utility.getLoginDetails();
+    let apiBaseUrl = Utility.apiBaseUrl;
+
+    apiBaseUrl += "/student/all-applications";
+
+    fetch(apiBaseUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json',
+            Authorization0: `Bearer ${loginDetails.loginToken}`
+        }
+    }).then(response => {
+        if (response.status == 403 || response.status == 401) {
+            showMessage("Please login!", "error");
+            redirectToLoginPage();
+            return;
+        } else if (response.status == 500) {
+            showMessage("Server is offline!", "error");
+            return;
+        } else if (!response.ok) {
+            showMessage("Something went wrong!", "error");
+            return;
+        }
+        return response.json();
+    }).then(data => {
+        Student.appendStudents(data);
+        console.log(data);
+    }).catch(error => {
+        Utility.showMessage(error.message, "error");
+    })
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     loadInstitutesPage();
@@ -49,5 +84,13 @@ document.addEventListener('DOMContentLoaded', () => {
     instituteNav.addEventListener("click", (e) => {
         loadInstitutesPage();
         fetchAllInstitutes();
+    })
+
+    let studentsNav = document.getElementById("students-nav");
+    
+
+    studentsNav.addEventListener("click", (e) => {
+        loadStudentsPage();
+        fetchStudents();
     })
 });
