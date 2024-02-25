97feb42bb5eab3909a619338b8cab131b0ebfa6d
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-25 23:46:20 +0200

    list of institutes fund allocation

--- js/pageLoader.js
+++ js/pageLoader.js
@@ -22,7 +22,7 @@ function loadInstitutesPage() {
         <section id="header">
         </section>
         <section id="add-institute-section">
-            <button id="add-institute-button"><i class="fa fa-plus" aria-hidden="true"></i> INSTITUTE</button>
+            <button id="add-institute-button" class="add-button"><i class="fa fa-plus" aria-hidden="true"></i> INSTITUTE</button>
         </section>
         <section id="filter-section">
             <h2>INSTITUTES</h2>
@@ -38,7 +38,6 @@ function loadInstitutesPage() {
                         <th scope="col">Reviewer</th>
                         <th scope="col">Status</th>
                         <th scope="col"></th>
-                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody id="results">
@@ -52,6 +51,38 @@ function loadInstitutesPage() {
 
 function loadFundingPage() {
     activeNav("funding");
+    let main = document.getElementById("main-section");
+    main.innerHTML = `
+        <section id="header">
+        </section>
+        <section id="add-institute-section">
+            <button id="add-funding-button" class="add-button"><i class="fa fa-plus" aria-hidden="true"></i> NEW FUNDING</button>
+            <article id="amount">
+                <h3>Fund Amount: <span id="fund-amount">R 1 000 000.00</span></h3>
+                <h3>Remainning: <span id="remaining-fund">R 2 000.00</span></h3>
+            </article>
+        </section>
+        <section id="filter-section">
+            <h2>FUNDED INSTITUTES</h2>
+            <a href="#">
+                <i class="fa fa-sliders fa-2x"></i>
+            </a>
+        </section>
+        <section id="institutes">
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Institute</th>
+                        <th scope="col">Allocated Amount</th>
+                        <th scope="col">Remaining Amount</th>
+                        <th scope="col">Financial Date</th>
+                    </tr>
+                </thead>
+                <tbody id="results">
+                </tbody>
+            </table>
+        </section>
+    `;
 }
 
 
