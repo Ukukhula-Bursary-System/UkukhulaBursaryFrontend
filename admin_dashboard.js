cb8b59e40d89d97eb0f737a2d1de7999ee1302d8
AndileCartist <andilemkhizekhabazela@gmail.com>
2024-02-29 11:54:53 +0200

    added new iframes and functinality

--- admin_dashboard.js
+++ admin_dashboard.js
@@ -1,11 +1,10 @@
 function toggleIframe(iframeId) {
-    // Hide all iframes
     document.getElementById('institutes').style.display = 'none';
     document.getElementById('funding').style.display = 'none';
     document.getElementById('students').style.display = 'none';
     document.getElementById('admins').style.display = 'none';
 
-    // Show the selected iframe
+
     document.getElementById(iframeId).style.display = 'block';
 }
 
@@ -13,4 +12,8 @@ function setupPage() {
     document.getElementById('funding').style.display = 'none';
     document.getElementById('admins').style.display = 'none';
     document.getElementById('students').style.display = 'none';
+}
+
+function logout() {
+    window.location.replace("/");
 }
--- components/admins/admins.js
+++ components/admins/admins.js
@@ -37,4 +37,57 @@ function appendAdmin(data) {
     }
 }
 
-export {fetchAllAdmins}
+function getValuesForNewAdmin() {
+    let firstName = document.getElementById("first-name");
+    let lastName = document.getElementById("last-name");
+    let phoneNumber = document.getElementById("phone-number");
+    let email = document.getElementById("email");
+    let role = document.getElementById("role");
+
+    return {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        phoneNumber: phoneNumber.value,
+        email: email.value,
+        roleId: role.value
+    }
+}
+
+export function addAdmin() {
+    let loginDetails = Utility.getLoginDetails();
+    let apiBaseUrl = Utility.apiBaseUrl;
+
+    apiBaseUrl += "/user";
+    let user = getValuesForNewAdmin();
+
+    fetch(apiBaseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json',
+            Authorization: `Bearer ${loginDetails.loginToken}`
+        },
+        body: JSON.stringify(user)
+    }).then(response => {
+        return Utility.handleErrorResponse(response);
+    }).then(data => {
+        Utility.showMessage(`User with email '${user.email}' successfully added!`)
+    }).catch(error => {
+        Utility.showMessage(error.message, "error");
+    })
+}
+
+export function toggleElement(elment) {
+    document.getElementById('admin-page').style.display = 'none';
+    document.getElementById('add-admin').style.display = 'none';
+
+    document.getElementById(elment).style.display = 'block';
+}
+function setupPage() {
+    document.getElementById('add-admin').style.display = 'none';
+}
+
+window.addAdmin = addAdmin
+window.toggleElement = toggleElement
+
+export { fetchAllAdmins, setupPage }
