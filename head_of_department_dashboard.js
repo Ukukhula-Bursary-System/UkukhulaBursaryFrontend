513f32347c6d530cc49ff7b521a63f58977734c3
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-03-01 13:50:17 +0200

    hod funding page done

--- head_of_department_dashboard.js
+++ head_of_department_dashboard.js
@@ -1,63 +1,16 @@
-import * as Funding from "./js/funding.js"
-import * as Applications from "./js/applications.js"
-import * as Utils from "./js/admin_dashboard_utility.js";
+function toggleIframe(iframeId) {
+    document.getElementById('funding').style.display = 'none';
+    document.getElementById('students').style.display = 'none';
 
-
-function loadPage(nav) {
-    const navs = ["funding", "applications", "add-application"];
-
-    for (let i = 0; i < navs.length; i++) {
-        if (nav == navs[i]) {
-            document.getElementById(nav).style.display = 'block';
-        } else {
-            document.getElementById(navs[i]).style.display = 'none';
-        }
-    }
+    document.getElementById(iframeId).contentDocument.location.reload(true);
+    document.getElementById(iframeId).style.display = 'block';
 }
 
+function setupPage() {
+    document.getElementById('funding').style.display = 'block';
+    document.getElementById('students').style.display = 'none';
+}
 
 function logout() {
     window.location.replace("/");
 }
-
-
-// Page set up
-document.addEventListener('DOMContentLoaded', function () {
-    loadPage("funding");
-    Utils.setLoggedInUser();
-
-    let loginDetails = Utils.getLoginDetails();
-    Funding.fetchAllInstitutesAllocatedFunds(loginDetails.institute);
-
-    let date = new Date();
-    Funding.fetchAllInstitutesAllocatedFunds(loginDetails.institute, date.getFullYear());
-
-    let fundingNav = document.getElementById("funding-nav");
-    let applicationsNav = document.getElementById("applications-nav");
-    let logoutNav = document.getElementById("logout-nav");
-
-    fundingNav.onclick = (e)=> {
-        loadPage("funding");
-        Funding.fetchAllInstitutesAllocatedFunds(loginDetails.institute);
-        let date = new Date();
-        Funding.fetchAllInstitutesAllocatedFunds(loginDetails.institute, date.getFullYear());
-    };
-
-
-    applicationsNav.onclick = (e) => {
-        loadPage("applications");
-        Applications.fetchAllAppicationsForHOD();
-
-        let addStudentApplication = document.getElementById("add-application-button");
-        addStudentApplication.onclick = (e) => {
-            loadPage("add-application");
-        }
-    };
-
-    logoutNav.onclick = (e) => {
-        logout();
-    };
-});
-
-
-
