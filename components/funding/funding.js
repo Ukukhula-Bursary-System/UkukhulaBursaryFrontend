269ea743fedb6e68c264c2246760798c57a84eee
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-03-02 18:11:19 +0200

    allocating institute funding

--- components/funding/funding.js
+++ components/funding/funding.js
@@ -1,18 +1,45 @@
 import * as Utility from "../../js/admin_dashboard_utility.js";
 
+const MzansiRand = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'ZAR',
+});
+
 
 function appendFunds(data) {
     let results = document.getElementById("results");
 
     results.innerHTML = '';
-    for (let i = 0; i < data.length; i++) {
-        results.innerHTML += `
+    if (data != undefined) {
+        for (let i = 0; i < data.length; i++) {
+            results.innerHTML += `
+                <tr class="table-rows">
+                    <td data-label="Institute">${data[i]["instituteName"]}</td>
+                    <td data-label="Allocated Amount">${MzansiRand.format(data[i]["allocatedAmount"])}</td>
+                    <td data-label="Remaining Amount">${MzansiRand.format(data[i]["allocatedRemainingAmount"])}</td>
+                    <td data-label="Financial Date">${data[i]["financialDate"]}</td>
+                </tr>
+            `;
+        }
+    } else {
+        results.innerHTML = `
             <tr class="table-rows">
-                <td data-label="Institute">${data[i]["instituteName"]}</td>
-                <td data-label="Allocated Amount">${data[i]["allocatedAmount"]}</td>
-                <td data-label="Remaining Amount">${data[i]["allocatedRemainingAmount"]}</td>
-                <td data-label="Financial Date">${data[i]["financialDate"]}</td>
+                <td data-label="Institute">No record was found.</td>
+                <td data-label="Allocated Amount"></td>
+                <td data-label="Remaining Amount"></td>
+                <td data-label="Financial Date"></td>
             </tr>
+        `;
+    }
+}
+
+
+function appendUniversitiesOptions(data) {
+    let universityOptions = document.getElementById("universities");
+
+    for (let i = 0; i < data.length; i++) {
+        universityOptions.innerHTML += `
+            <option value=${data[i]["instituteId"]}>${data[i]["instituteName"]}</option>
         `;
     }
 }
@@ -23,8 +50,8 @@ function appendInstituteApplication(data) {
     let remainingAmount = document.getElementById("remaining-amount");
     let instituteName = document.getElementById("institute-name");
 
-    fundAmount.innerText = `R ${data["allocatedAmount"]}`;
-    remainingAmount.innerText = `R ${data["allocatedRemainingAmount"]}`;
+    fundAmount.innerText = `${MzansiRand.format(data["allocatedAmount"])}`;
+    remainingAmount.innerText = `${MzansiRand.format(data["allocatedRemainingAmount"])}`;
     instituteName.innerText = data["instituteName"];
 }
 
@@ -38,8 +65,6 @@ function fetchAllInstitutesAllocatedFunds(instituteId, year) {
     if (instituteId !== undefined) {
         apiBaseUrl += "/single-institute/" + instituteId;
     }
-
-    console.log(apiBaseUrl);
 
     if (year !== "" && year !== undefined) {
         apiBaseUrl += "/" + year;
@@ -55,7 +80,7 @@ function fetchAllInstitutesAllocatedFunds(instituteId, year) {
     }).then(response => {
         return Utility.handleErrorResponse(response);
     }).then(data => {
-        if (year !== "" && year !== undefined) {
+        if (year !== "" && year !== undefined && instituteId !== undefined) {
             appendInstituteApplication(data[0]);
         } else {
             appendFunds(data);
@@ -65,10 +90,100 @@ function fetchAllInstitutesAllocatedFunds(instituteId, year) {
     })
 }
 
+
+function fetchAllApprovedUniversities() {
+    let loginDetails = Utility.getLoginDetails();
+    let apiBaseUrl = Utility.apiBaseUrl;
+
+    apiBaseUrl += "/institute/" + 2;
+
+    fetch(apiBaseUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json',
+            Authorization: `Bearer ${loginDetails.loginToken}`
+        }
+    }).then(response => {
+        return Utility.handleErrorResponse(response);
+    }).then(data => {
+        appendUniversitiesOptions(data);
+    }).catch(error => {
+        Utility.showMessage(error.message, "error");
+    })
+}
+
+
 function displayValue(value) {
     document.getElementById('display-value').value = value;
 }
 
 window.displayValue = displayValue
 
-export {fetchAllInstitutesAllocatedFunds}
+
+function getValuesForNewFunding() {
+    let amount = document.getElementById("funding-amount");
+    let instituteId = document.getElementById("universities");
+
+    if (amount.value == "") {
+        throw "Amount should be greater than zero.";
+    }
+
+    if (instituteId.value == "-1") {
+        throw "Choose an institute.";
+    }
+
+    return {
+        allocatedAmount: amount.value,
+        instituteId: instituteId.value
+    }
+}
+
+
+function addFunding() {
+    let loginDetails = Utility.getLoginDetails();
+    let apiBaseUrl = Utility.apiBaseUrl;
+
+    apiBaseUrl += "/institute/funds";
+    
+    try {
+        let newFunding = getValuesForNewFunding();
+
+        fetch(apiBaseUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: 'application/json',
+                Authorization: `Bearer ${loginDetails.loginToken}`
+            },
+            body: JSON.stringify(newFunding)
+        }).then(response => {
+            return Utility.handleErrorResponse(response);
+        }).then(data => {
+            Utility.showMessage(data["message"])
+        }).catch(error => {
+            Utility.showMessage(error.message, "error");
+        });
+    } catch(error) {
+        Utility.showMessage(error, "error");
+    }
+}
+
+
+function toggleElement(elment) {
+    document.getElementById('funding-page').style.display = 'none';
+    document.getElementById('add-funding').style.display = 'none';
+
+    document.getElementById(elment).style.display = 'block';
+}
+
+
+function setupPage() {
+    document.getElementById('add-funding').style.display = 'none';
+}
+
+
+window.addFunding = addFunding
+window.toggleElement = toggleElement
+
+export { fetchAllInstitutesAllocatedFunds, addFunding, toggleElement, setupPage, fetchAllApprovedUniversities }
